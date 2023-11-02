1169cf061c37fa7d2caa787f8087d5df27812d6a
Joseph Kim <josephikim2@gmail.com>
2023-11-01 20:39:04 -0700

    add interview question easy-14

--- Interview Questions/Grind75/Easy/13_implementQueueUsingStacks.js
+++ Interview Questions/Grind75/Easy/13_implementQueueUsingStacks.js
@@ -38,13 +38,21 @@
 
 /***********************************/
 
+// NOTE FOR JAVASCRIPT
+// JS spec does not natively have a stack data structure
+// You can implement stack-like structure using arrays, or use a 3rd party library eg Immutable.js or Queue.js
+// These library usually include dequeue function that runs in amortized constant time. As a result, for larger queues, it can be significantly faster than using arrays.
+
+/***********************************/
+
 // Optimal solution
 
-// Time complexity = O(N) N = list length
-// Space complexity = O(N) = map object with N entries
+// Allows for amortized O(1) time complexity (see above)
+// Use two stacks to implement a FIFO queue
 
 var MyQueue = function () {
-	this.queue = [];
+	this.stack1 = [];
+	this.stack2 = [];
 };
 
 /**
@@ -52,29 +60,133 @@ var MyQueue = function () {
  * @return {void}
  */
 MyQueue.prototype.push = function (x) {
-	this.queue.push(x);
+	this.stack1.push(x);
 };
 
 /**
  * @return {number}
  */
 MyQueue.prototype.pop = function () {
-	return this.queue.shift();
+	this.rearrange();
+	return this.stack2.pop();
 };
 
 /**
  * @return {number}
  */
 MyQueue.prototype.peek = function () {
-	return this.queue[0];
+	this.rearrange();
+	return this.stack2[this.stack2.length - 1];
 };
 
 /**
  * @return {boolean}
  */
 MyQueue.prototype.empty = function () {
-	return this.queue.length < 1;
+	return this.stack1.length === 0 && this.stack2.length === 0;
 };
+
+// Removes items from stack 1 and inserts them in opposite order into stack 2
+MyQueue.prototype.rearrange = function () {
+	if (this.stack2.length === 0) {
+		while (this.stack1.length > 0) {
+			this.stack2.push(this.stack1.pop());
+		}
+	}
+};
+
+// Manual implementation of a JS stack (optional)
+// var MyStack = function () {
+// 	this.items = [];
+// };
+
+// /**
+//  * @param {number} x
+//  * @return {void}
+//  */
+// MyStack.prototype.add = function (x) {
+// 	return this.items.push(x);
+// };
+
+// /**
+//  * @return {number}
+//  */
+// MyStack.prototype.remove = function () {
+// 	if (this.items.length > 0) {
+// 		return this.items.pop();
+// 	}
+// };
+
+// /**
+//  * @return {number}
+//  */
+// MyStack.prototype.peek = function () {
+// 	return this.items[this.items.length - 1];
+// };
+
+// /**
+//  * @return {boolean}
+//  */
+// MyStack.prototype.isEmpty = function () {
+// 	return this.items.length == 0;
+// };
+
+// /**
+//  * @return {number}
+//  */
+// MyStack.prototype.size = function () {
+// 	return this.items.length;
+// };
+
+// /**
+//  * @return {void}
+//  */
+// MyStack.prototype.clear = function () {
+// 	this.items = [];
+// };
+
+/***********************************/
+
+// Naive solution (uses JS built-in dynamic array)
+
+// Time complexity = O(N) N = list length
+// 	push() = O(1)
+// 	shift() = O(N)
+//  access ie arr[x] = O(1)
+//  arr.length = O(1)
+
+// var MyQueue = function () {
+// 	this.items = [];
+// };
+
+// /**
+//  * @param {number} x
+//  * @return {void}
+//  */
+// MyQueue.prototype.push = function (x) {
+// 	this.items.push(x);
+// };
+
+// /**
+//  * @return {number}
+//  */
+// MyQueue.prototype.pop = function () {
+// 	return this.items.shift();
+// };
+
+// /**
+//  * @return {number}
+//  */
+// MyQueue.prototype.peek = function () {
+// 	return this.items[0];
+// };
+
+// /**
+//  * @return {boolean}
+//  */
+// MyQueue.prototype.empty = function () {
+// 	return this.items.length < 1;
+// };
 
 /**
  * Your MyQueue object will be instantiated and called as such:
@@ -85,12 +197,12 @@ MyQueue.prototype.empty = function () {
  * var param_4 = obj.empty()
  */
 
-var obj = new MyQueue();
-obj.push(1);
-console.dir(obj);
-obj.push(2);
-console.dir(obj);
-console.dir(obj.peek());
-obj.pop();
-console.dir(obj.empty());
-console.dir(obj);
+var queue = new MyQueue();
+queue.push(1);
+console.dir(queue);
+queue.push(2);
+console.dir(queue);
+console.dir(queue.pop());
+console.dir(queue);
+console.dir(queue.empty());
+console.dir(queue.peek());
