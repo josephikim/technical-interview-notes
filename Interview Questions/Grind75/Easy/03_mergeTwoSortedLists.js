8ed8bf65847f2410037a0a13c74734df1f5588da
Joseph Kim <josephikim2@gmail.com>
2023-11-09 01:26:58 -0800

    update interview question easy 3

--- Interview Questions/Grind75/Easy/03_mergeTwoSortedLists.js
+++ Interview Questions/Grind75/Easy/03_mergeTwoSortedLists.js
@@ -68,3 +68,38 @@ var mergeTwoLists = function (list1, list2) {
 
 	return merged.next;
 };
+
+/***********************************/
+
+// Recursive solution
+// Time commplexity = O(n) worst case
+// n = l1.length + l2.length
+//
+// Space complexity = O(1)
+//
+// Use recursion to stitch sublists together one node at a time
+
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} list1
+ * @param {ListNode} list2
+ * @return {ListNode}
+ */
+var mergeTwoLists = function (list1, list2) {
+	if (!list1) return list2;
+	if (!list2) return list1;
+
+	if (list1.val < list2.val) {
+		list1.next = mergeTwoLists(list1.next, list2);
+		return list1;
+	} else {
+		list2.next = mergeTwoLists(list1, list2.next);
+		return list2;
+	}
+};
