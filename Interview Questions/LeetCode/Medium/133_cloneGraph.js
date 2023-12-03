bfcff12eeced2b06b65d5c3319aaefb8b39eabff
Joseph Kim <josephikim2@gmail.com>
2023-12-02 23:13:05 -0800

    add leetcode 133

--- Interview Questions/LeetCode/Medium/133_cloneGraph.js
+++ Interview Questions/LeetCode/Medium/133_cloneGraph.js
@@ -49,12 +49,19 @@
 
 /***********************************/
 
-// Iterative solution
+// Iterative solution (BFS)
 
-// Time complexity =
-// Space complexity =
+// Time complexity = O(n) => n = num of nodes
+// Space complexity = O(n) => clones and traversed are both O(n)
 
-// 1.
+// 1. init clone Map<node, cloneNode>
+// 2. init traversed Set()
+// 3. use BFS to traverse the graph
+// 4. use the Set to avoid circular path
+// 5. traverse queue starting with head
+// 6. for each neighbor, if not cloned, create a new node and connect it to the current node
+// 7. if not traversed, push neighbor into queue
+// 8. return cloned input node
 
 /**
  * // Definition for a Node.
@@ -68,4 +75,97 @@
  * @param {Node} node
  * @return {Node}
  */
-var cloneGraph = function (node) {};
+var cloneGraph = function (node) {
+	if (!node) return null;
+
+	const clones = new Map();
+	const traversed = new Set();
+	const queue = [node];
+
+	clones.set(node, new Node(node.val));
+
+	while (queue.length > 0) {
+		const head = queue.shift();
+
+		if (traversed.has(head)) {
+			continue;
+		}
+		const clonedHead = clones.get(head);
+		for (let neighbor of head.neighbors) {
+			if (clones.has(neighbor)) {
+				clonedHead.neighbors.push(clones.get(neighbor));
+			} else {
+				const clonedNeighbor = new Node(neighbor.val);
+				clonedHead.neighbors.push(clonedNeighbor);
+				clones.set(neighbor, clonedNeighbor);
+			}
+
+			if (!traversed.has(neighbor)) {
+				queue.push(neighbor);
+			}
+		}
+		traversed.add(head);
+	}
+	return clones.get(node);
+};
+
+/***********************************/
+
+// Iterative solution (DFS)
+
+// Time complexity = O(n) => n = num of nodes
+// Space complexity = O(n) => clones and traversed are both O(n)
+
+// 1. init clone Map<node, cloneNode>
+// 2. init traversed Set()
+// 3. use DFS to traverse the graph
+// 4. use the Set to avoid circular path
+// 5. traverse stack starting with top
+// 6. for each neighbor, if not cloned, create a new node and connect it to the current node
+// 7. if not traversed, push neighbor into stack
+// 8. return cloned input node
+
+/**
+ * // Definition for a Node.
+ * function Node(val, neighbors) {
+ *    this.val = val === undefined ? 0 : val;
+ *    this.neighbors = neighbors === undefined ? [] : neighbors;
+ * };
+ */
+
+/**
+ * @param {Node} node
+ * @return {Node}
+ */
+var cloneGraph = function (node) {
+	if (!node) return null;
+
+	const clones = new Map();
+	const traversed = new Set();
+	const stack = [node];
+
+	clones.set(node, new Node(node.val));
+
+	while (queue.length > 0) {
+		const top = stack.pop();
+		if (traversed.has(top)) {
+			continue;
+		}
+		const clonedHead = clones.get(top);
+		for (let neighbor of top.neighbors) {
+			if (clones.has(neighbor)) {
+				clonedHead.neighbors.push(clones.get(neighbor));
+			} else {
+				const clonedNeighbor = new Node(neighbor.val);
+				clonedHead.neighbors.push(clonedNeighbor);
+				clones.set(neighbor, clonedNeighbor);
+			}
+
+			if (!traversed.has(neighbor)) {
+				stack.push(neighbor);
+			}
+		}
+		traversed.add(top);
+	}
+	return clones.get(node);
+};
