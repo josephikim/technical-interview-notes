18e5bd819c53bc727979073c896c0eec8291249c
Joseph Kim <josephikim2@gmail.com>
2023-12-13 18:35:29 -0800

    add closures

--- JavaScript/Closures.js
+++ JavaScript/Closures.js
@@ -14,8 +14,8 @@
 var passed = 3;
 
 var addTo = function () {
-  var inner = 2;
-  return passed + inner;
+	var inner = 2;
+	return passed + inner;
 };
 console.log(addTo()); // 5
 
@@ -27,19 +27,19 @@ console.log(addTo()); // 6
 const buttons = document.querySelectorAll("button");
 
 buttons.forEach((button, i) => {
-  button.addEventListener("click", (e) => {
-    console.log(i);
-  });
+	button.addEventListener("click", (e) => {
+		console.log(i);
+	});
 }); // 0, 1, 2
 
 //// Level 3 - INNER FUNCTION CALLED FROM THE OUTSIDE
 //// The returned function uses the lexical scope of makeFunc()
 function makeFunc() {
-  const name = "Mozilla";
-  function displayName() {
-    console.log(name);
-  }
-  return displayName;
+	const name = "Mozilla";
+	function displayName() {
+		console.log(name);
+	}
+	return displayName;
 }
 const myFunc = makeFunc();
 myFunc(); // "Mozilla"
@@ -47,10 +47,10 @@ myFunc(); // "Mozilla"
 //// Level 4 - INNER FUNCTION WIH CUSTOM SCOPED DATA
 //// Each returned function can be tied to unique data via the outer function's lexical scope
 var addTo = function (passed) {
-  var add = function (inner) {
-    return passed + inner;
-  };
-  return add;
+	var add = function (inner) {
+		return passed + inner;
+	};
+	return add;
 };
 var addThree = new addTo(3); // 'new' keyword creates a new function object (instance), thus a new 'this'
 var addFour = new addTo(4);
@@ -62,14 +62,14 @@ console.log(addFour(1)); // 5
 //// The IIFE runs only once, but the returned function is still tied to data in the IIFE's scope, thus making this pattern useful for constructing a counter.
 //// NOTE: 'counter' cannot be modified from the global scope.
 
-const add = (function () {
-  let counter = 0;
-  return function () {
-    counter += 1;
-    return counter;
-  };
+const increment = (function () {
+	let counter = 0;
+	return function () {
+		counter += 1;
+		return counter;
+	};
 })();
 
-add(); // 1
-add(); // 2
-add(); // 3
+increment(); // 1
+increment(); // 2
+increment(); // 3
