d47cfef90b291147362a12de671caa7a25fe3136
Joseph Kim <josephikim2@gmail.com>
2024-02-06 21:05:46 -0800

    update leetcode 24

--- Interview Questions/LeetCode/Medium/24_swapNodesInPairs.js
+++ Interview Questions/LeetCode/Medium/24_swapNodesInPairs.js
@@ -4,10 +4,12 @@
 
 // Input: head = [1,2,3,4]
 // Output: [2,1,4,3]
+
 // Example 2:
 
 // Input: head = []
 // Output: []
+
 // Example 3:
 
 // Input: head = [1]
@@ -18,9 +20,30 @@
 // The number of nodes in the list is in the range [0, 100].
 // 0 <= Node.val <= 100
 
-/***********************************/
+/**********************************
 
-// Non-recursive solution - This is more efficient space-wise than recursive solution (may avoid stack overflow)
+O(n) - Iterative with pointers
+
+1. Handle base case (!head || !head.next)
+2. Init pointer newHead and point to head.next (this will be the new head)
+3. Init pointer currNode and point it to head 
+4. Init pointer prevNode = null
+5. Each while loop will essentially process two nodes at a time:
+6. Create pointer clone of node 2, attach node1 to node3, attach clone to node1, attach prev node to clone, 
+7. set prev to curr and curr to curr.next // This moves while loop forward by two nodes
+
+O(n) - Recursion with pointer
+
+1. Handle base case (!head || !head.next)
+2. Init pointer as new ListNode (make a clone of head) and point it to head.next.next
+3. Make head.next the new head and point it to the pointer
+4. Swap next pair of nodes after pointer node and attach to head.next.next
+(Recurrence relationship: head.next.next = swapPairsRecursive(head.next.next))
+5. return head
+
+***********************************/
+
+// Iterative solution - This is more efficient space-wise than recursive solution
 
 // Time complexity = O(N/2) == O(N)
 // Space complexity = O(1)
@@ -44,7 +67,7 @@ var swapPairs = function (head) {
 		return head;
 	}
 
-	let newNode = head.next;
+	let newHead = head.next;
 
 	let currNode = head;
 	let prevNode = null;
@@ -63,12 +86,12 @@ var swapPairs = function (head) {
 		currNode = currNode.next; // update currentNode pointer
 	}
 
-	return newNode;
+	return newHead;
 };
 
 /***********************************/
 
-// Recursive solution
+// Recursive solution (may cause stack overflow)
 
 // Time complexity = O(N/2) == O(N)
 // Space complexity = O(N/2) == O(N)
@@ -88,10 +111,15 @@ var swapPairs = function (head) {
  */
 var swapPairsRecursive = function (head) {
 	if (!head || !head.next) return head;
+	// Make a clone of head and point it to head.next.next
 	let pointer = new ListNode(head.val, head.next);
 	pointer.next = head.next.next;
+
+	// Make head.next the new head and point it to the pointer
 	head = head.next;
 	head.next = pointer;
+
+	// swap next pair of nodes after pointer node
 	head.next.next = swapPairsRecursive(head.next.next);
 	return head;
 };
